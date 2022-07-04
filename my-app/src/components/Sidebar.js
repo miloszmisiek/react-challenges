9bcbfd2b094d5bebc30b0b8ec37ac8405446da23
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-07-04 17:13:35 +0200

    Edit Sidebar.js, add css to Sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,6 @@
+import React from"react";
+import css from "./css/Sidebar.module.css"
+
+const Sidebar = () => <div className={css.sidebar}><a>My Photos</a><a>My Illustrations</a><a>My Paintings</a></div> ;
+
+export default Sidebar
