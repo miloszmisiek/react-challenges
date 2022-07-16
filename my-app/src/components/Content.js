7555901cfcb8aea173d80222fff2fd8d08a6a0d7
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-07-16 15:37:39 +0100

    Add search bard functionality

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css";
 import { savedPosts } from "../posts.json"
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isLoaded: false,
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts
+            })
+        }, 2000)
+    }
+
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter(post => post.name.toLowerCase().includes(name))
+        this.setState({ posts: filteredPosts })
     }
 
     render() {
@@ -14,9 +35,19 @@ export class Content extends Component {
             <div>
                 <div className={css.TitleBar}>
                     <h1>My Posts</h1>
+                    <form>
+                        <label htmlFor='searchInput'>Search:</label>
+                        <input
+                            type="search"
+                            id='searchInput'
+                            placeholder='By Author'
+                            onChange={(e) => this.handleChange(e)}
+                        />
+                        <h4>posts found: {this.state.posts.length}</h4>
+                    </form>
                 </div>
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                    {this.state.isLoaded ? <PostItem savedPosts={this.state.posts} /> : <Loader />}
                 </div>
             </div>
         )
