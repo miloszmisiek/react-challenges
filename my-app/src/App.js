bbe78ab5e408a8eaf4c075148aa393fbacdf97c2
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-07-20 20:30:07 +0200

    Add ContentAPIHooks.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,8 @@ import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 // import Content from './components/Content';
 // import ContentHooks from './components/ContentHooks';
-import ContentAPI from './components/ContentAPI';
+// import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from './components/ContentAPIHooks';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Sidebar />
       {/* <NavBarSimple /> */}
       <NavBarForm />
-      <ContentAPI />
+      <ContentAPIHooks />
 
     </div>
   );
